6169ec86b2ae740a31084ee5c931a55fce2fd93b
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-07-14 13:06:57 +0500

    feat: add fragments learning module

---
+++ Learning_Fragments/app/src/main/java/com/example/learning_fragments/Fragments/secondFragment.kt
@@ -0,0 +1,21 @@
+package com.example.learning_fragments.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.learning_fragments.R
+
+class secondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+
+}
